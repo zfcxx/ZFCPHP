fe76c1fe64c28929164498a04f2904511517c0ca
zfcxx <1472192929@qq.com>
2018-06-10 10:37:45 +0800

    first commit

--- Front/Controller/index.class.php
+++ Front/Controller/index.class.php
@@ -6,13 +6,15 @@ class Contorller_index extends Controller_B
 {
 
 	public function index(){
-
+		$this->start_view();//压缩处理
 		$view=new View();
 		$db=new ZFC_DB;
 		$arr=['title'=>'ZFCPHP框架','text'=>'欢迎使用ZFCPHP'];
 		$view->assign('arr',$arr);
 		$view->display('index');
-
+		$html = ob_get_contents();
+		$this->end_view($html);//压缩输出
+		
 	}
 
 
--- ZFCPHP/System/Controller_B.php
+++ ZFCPHP/System/Controller_B.php
@@ -3,7 +3,18 @@
 class Controller_B{
 
 	function __construct (){
-
+	}
+	//开始http压缩
+	function start_view(){
+		header ("Content-Type: text/html"); 
+        header ("Content-Encoding: gzip");
+		ob_start();
+	}
+	//输出http压缩
+	function end_view($html){
+		ob_end_clean();
+		$json= gzencode($html, 9, FORCE_GZIP);//$gzip ? FORCE_GZIP : FORCE_DEFLATE);  
+        echo $json;
 	}
 
 }
